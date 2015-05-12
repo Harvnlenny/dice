3e7735911002ad7964083bb3dc8e9213905159a1
Aaron Harvey <harvnlenny@gmail.com>
2015-05-11 22:31:56 -0400

    updated game.rb

--- game.rb
+++ game.rb
@@ -1,9 +1,49 @@
 require "./dice"
 
-class Player
-end
-
+class Player 
+  def initialize (name, dice) 
+    @name = name
+    @dice = dice
+  end
+  
+  def dice
+    @dice
+  end  
+end  
 
 class Game
-  # Players roll their dice until someone wins
-end
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @turn = 0
+  end
+
+  def play!
+    tie = true
+    until tie == false
+      player_1_roll = @player_1.dice.roll
+      player_2_roll = @player_2.dice.roll
+      if player_1_roll > player_2_roll
+        @winner = @player_1
+        tie = false
+        @turn += 1
+      elsif
+        player_2_roll > player_1_roll
+        @winner = @player_2
+        tie = false
+        @turn += 1
+      else
+        @turn += 1  
+      end
+    end
+  end
+
+  def winner
+    @winner    
+  end 
+
+  def turn
+    @turn
+  end  
+end   
+
